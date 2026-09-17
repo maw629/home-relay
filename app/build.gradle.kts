1987c7551dbec8e5322c4ac9e51d333c61b1f1aa
Hardy Nguyen <maw.signup@gmail.com>
2026-09-17 09:32:10 +0700

    build: bump versionCode - 4 (#28)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "app.maw629.homerelay"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
